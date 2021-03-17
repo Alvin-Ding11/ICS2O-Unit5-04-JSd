c574deab52a0bf30ad87b139447627eea1e8a39a
AlvinDing11 <alvin.ding@ocsbstudent.ca>
2021-03-17 14:41:10 +0000

    js fixes and css added

--- script.js
+++ script.js
@@ -8,22 +8,18 @@ function image () {
   document.getElementById('panda').src = 'images/Pandaday.jfif'
 }
 //This function changes the image of the Panda on the basketball to another image of a panda
-
 function text () {
   document.getElementById('para').innerHTML = 'This is the altered text after pressing the button'
 }
 //This function changes the text that is shown in the paragraph
-
 function hide () {
   document.getElementById('para').style.display = 'none'
 }
 //This function makes the text disapper on the screen
-
 function appear () {
   document.getElementById('para').style.display = 'block'
 }
 //This function can make the text reapper on the screen once it has been removed
-
 function back () {
   document.body.style.backgroundColor = 'blue'
 }
